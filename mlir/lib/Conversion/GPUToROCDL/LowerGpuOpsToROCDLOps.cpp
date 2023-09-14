075c0bcf5a7d78c987b9c3e631536434c3c159c5
Groverkss <groverkss@gmail.com>
2023-09-14 21:41:19 +0530

    Fast static shuffling

--- mlir/lib/Conversion/GPUToROCDL/LowerGpuOpsToROCDLOps.cpp
+++ mlir/lib/Conversion/GPUToROCDL/LowerGpuOpsToROCDLOps.cpp
@@ -54,6 +54,107 @@ namespace mlir {
 
 using namespace mlir;
 
+enum class DppCtrl : unsigned {
+  DppQuadPerm0000 = 0x000,
+  DppQuadPerm1111 = 0x055,
+  DppQuadPerm2222 = 0x0AA,
+  DppQuadPerm3333 = 0x0FF,
+  DppQuadPerm1032 = 0x0B1,
+  DppQuadPerm2301 = 0x04E,
+  DppQuadPerm0123 = 0x0E4,
+  DppQuadPerm3210 = 0x01B,
+  DppRowSl1 = 0x101,
+  DppRowSl2 = 0x102,
+  DppRowSl3 = 0x103,
+  DppRowSl4 = 0x104,
+  DppRowSl5 = 0x105,
+  DppRowSl6 = 0x106,
+  DppRowSl7 = 0x107,
+  DppRowSl8 = 0x108,
+  DppRowSl9 = 0x109,
+  DppRowSl10 = 0x10A,
+  DppRowSl11 = 0x10B,
+  DppRowSl12 = 0x10C,
+  DppRowSl13 = 0x10D,
+  DppRowSl14 = 0x10E,
+  DppRowSl15 = 0x10F,
+  DppRowSr1 = 0x111,
+  DppRowSr2 = 0x112,
+  DppRowSr3 = 0x113,
+  DppRowSr4 = 0x114,
+  DppRowSr5 = 0x115,
+  DppRowSr6 = 0x116,
+  DppRowSr7 = 0x117,
+  DppRowSr8 = 0x118,
+  DppRowSr9 = 0x119,
+  DppRowSr10 = 0x11A,
+  DppRowSr11 = 0x11B,
+  DppRowSr12 = 0x11C,
+  DppRowSr13 = 0x11D,
+  DppRowSr14 = 0x11E,
+  DppRowSr15 = 0x11F,
+  DppRowRr1 = 0x121,
+  DppRowRr2 = 0x122,
+  DppRowRr3 = 0x123,
+  DppRowRr4 = 0x124,
+  DppRowRr5 = 0x125,
+  DppRowRr6 = 0x126,
+  DppRowRr7 = 0x127,
+  DppRowRr8 = 0x128,
+  DppRowRr9 = 0x129,
+  DppRowRr10 = 0x12A,
+  DppRowRr11 = 0x12B,
+  DppRowRr12 = 0x12C,
+  DppRowRr13 = 0x12D,
+  DppRowRr14 = 0x12E,
+  DppRowRr15 = 0x12F,
+
+  // WfSl and WfSr are not available on GFX10+.
+  DppWfSl1 = 0x130,
+  DppWfSr1 = 0x138,
+
+  DppRowMirror = 0x140,
+  DppRowHalfMirror = 0x141,
+
+  // RowBcast modes are not available on GFX10+.
+  DppRowBcast15 = 0x142,
+  DppRowBcast31 = 0x143,
+
+  // RowXmask and RowShare modes are only available on GFX10+.
+  DppRowShare0 = 0x150,
+  DppRowShare1 = 0x151,
+  DppRowShare2 = 0x152,
+  DppRowShare3 = 0x153,
+  DppRowShare4 = 0x154,
+  DppRowShare5 = 0x155,
+  DppRowShare6 = 0x156,
+  DppRowShare7 = 0x157,
+  DppRowShare8 = 0x158,
+  DppRowShare9 = 0x159,
+  DppRowShare10 = 0x15A,
+  DppRowShare11 = 0x15B,
+  DppRowShare12 = 0x15C,
+  DppRowShare13 = 0x15D,
+  DppRowShare14 = 0x15E,
+  DppRowShare15 = 0x15F,
+  DppRowXmask0 = 0x160,
+  DppRowXmask1 = 0x161,
+  DppRowXmask2 = 0x162,
+  DppRowXmask3 = 0x163,
+  DppRowXmask4 = 0x164,
+  DppRowXmask5 = 0x165,
+  DppRowXmask6 = 0x166,
+  DppRowXmask7 = 0x167,
+  DppRowXmask8 = 0x168,
+  DppRowXmask9 = 0x169,
+  DppRowXmask10 = 0x16A,
+  DppRowXmask11 = 0x16B,
+  DppRowXmask12 = 0x16C,
+  DppRowXmask13 = 0x16D,
+  DppRowXmask14 = 0x16E,
+  DppRowXmask15 = 0x16F,
+};
+
 /// Returns true if the given `gpu.func` can be safely called using the bare
 /// pointer calling convention.
 static bool canBeCalledWithBarePointers(gpu::GPUFuncOp func) {
@@ -64,7 +165,7 @@ static bool canBeCalledWithBarePointers(gpu::GPUFuncOp func) {
   return canBeBare;
 }
 
-Value getLaneId(ConversionPatternRewriter &rewriter, Location loc,
+Value getLaneId(RewriterBase &rewriter, Location loc,
                 const unsigned indexBitwidth) {
   auto int32Type = IntegerType::get(rewriter.getContext(), 32);
   Value zero = rewriter.createOrFold<arith::ConstantIntOp>(loc, 0, 32);
@@ -113,6 +214,213 @@ struct GPULaneIdOpToROCDL : ConvertOpToLLVMPattern<gpu::LaneIdOp> {
 struct GPUShuffleOpLowering : public ConvertOpToLLVMPattern<gpu::ShuffleOp> {
   using ConvertOpToLLVMPattern<gpu::ShuffleOp>::ConvertOpToLLVMPattern;
 
+  std::pair<Value, Value> createShuffle(RewriterBase &rewriter, Location loc,
+                                        Value srcLaneId, Value dstLaneId,
+                                        OpAdaptor adaptor) const {
+    auto int32Type = rewriter.getIntegerType(32);
+    auto boolType = rewriter.getIntegerType(1);
+    auto width = adaptor.getWidth();
+
+    Value zero = rewriter.create<LLVM::ConstantOp>(loc, int32Type, 0);
+    Value negwidth = rewriter.create<LLVM::SubOp>(loc, int32Type, zero, width);
+    Value add = rewriter.create<LLVM::AddOp>(loc, int32Type, srcLaneId, width);
+    Value widthOrZeroIfOutside =
+        rewriter.create<LLVM::AndOp>(loc, int32Type, add, negwidth);
+    Value isActiveSrcLane = rewriter.create<LLVM::ICmpOp>(
+        loc, LLVM::ICmpPredicate::slt, dstLaneId, widthOrZeroIfOutside);
+    Value selectDstLane = rewriter.create<LLVM::SelectOp>(loc, isActiveSrcLane,
+                                                          dstLaneId, srcLaneId);
+    Value two = rewriter.create<LLVM::ConstantOp>(loc, int32Type, 2);
+    Value dwordAlignedDstLane =
+        rewriter.create<LLVM::ShlOp>(loc, int32Type, selectDstLane, two);
+    Value initShflValue = adaptor.getValue();
+    if (adaptor.getValue().getType().isF32()) {
+      initShflValue =
+          rewriter.create<LLVM::BitcastOp>(loc, int32Type, initShflValue);
+    }
+
+    Value shflValue = rewriter.create<ROCDL::DsBpermuteOp>(
+        loc, int32Type, dwordAlignedDstLane, initShflValue);
+    if (adaptor.getValue().getType().isF32()) {
+      shflValue = rewriter.create<LLVM::BitcastOp>(
+          loc, adaptor.getValue().getType(), shflValue);
+    }
+
+    return std::make_pair(shflValue, isActiveSrcLane);
+  }
+
+  FailureOr<unsigned> getStaticMask(Value offset, Value width) const {
+    auto widthOp = dyn_cast_or_null<LLVM::ConstantOp>(width.getDefiningOp());
+    auto offsetOp = dyn_cast_or_null<LLVM::ConstantOp>(offset.getDefiningOp());
+    if (!widthOp || !offsetOp)
+      return failure();
+    auto widthIntAttr = dyn_cast<IntegerAttr>(widthOp.getValue());
+    auto offsetIntAttr = dyn_cast<IntegerAttr>(offsetOp.getValue());
+    if (!offsetIntAttr || !widthIntAttr)
+      return failure();
+    int32_t offsetInt32 = offsetIntAttr.getInt();
+    int32_t widthInt32 = widthIntAttr.getInt();
+    // Width needs to be same size as warp.
+    const int32_t kWarpSize = 32;
+    if (widthInt32 != kWarpSize)
+      return failure();
+    // Do zext to unsigned to represent mask.
+    return static_cast<unsigned>(offsetInt32);
+  }
+
+  std::pair<Value, Value> createShuffleXor(RewriterBase &rewriter, Location loc,
+                                           OpAdaptor adaptor) const {
+    auto int32Type = rewriter.getIntegerType(32);
+    auto boolType = rewriter.getIntegerType(1);
+    Value offset = adaptor.getOffset();
+    Value width = adaptor.getWidth();
+
+    bool canOptimize = false;
+    DppCtrl dppCtrl = DppCtrl::DppQuadPerm0000;
+
+    // Use dpp_mov if mask < 16
+    // Use permlanex16 if support or ds_swizzle, if 16 <= mask < 32
+    // Use ds_bpermute to handle more complex cases
+    auto maybeMask = getStaticMask(adaptor.getOffset(), adaptor.getWidth());
+    if (succeeded(maybeMask)) {
+      unsigned mask = maybeMask.value();
+      if (mask < 32) {
+        bool canOptimize = true;
+        switch (mask) {
+        case 0:
+          dppCtrl = DppCtrl::DppQuadPerm0123;
+          break;
+        case 1:
+          dppCtrl = DppCtrl::DppQuadPerm1032;
+          break;
+        case 2:
+          dppCtrl = DppCtrl::DppQuadPerm2301;
+          break;
+        case 3:
+          dppCtrl = DppCtrl::DppQuadPerm3210;
+          break;
+        case 7:
+          dppCtrl = DppCtrl::DppRowHalfMirror;
+        case 8:
+          dppCtrl = DppCtrl::DppRowRr8;
+        case 15:
+          dppCtrl = DppCtrl::DppRowMirror;
+          break;
+        default:
+          canOptimize = false;
+          break;
+        }
+
+        // TODO: Check if the backend supports DppRowXmask.
+        // Check if we can use DppRowXmask.
+        if (!canOptimize) {
+          canOptimize = true;
+          switch (mask) {
+          case 4:
+            dppCtrl = DppCtrl::DppRowXmask4;
+            break;
+          case 5:
+            dppCtrl = DppCtrl::DppRowXmask5;
+            break;
+          case 6:
+            dppCtrl = DppCtrl::DppRowXmask6;
+            break;
+          case 9:
+            dppCtrl = DppCtrl::DppRowXmask9;
+            break;
+          case 10:
+            dppCtrl = DppCtrl::DppRowXmask10;
+            break;
+          case 11:
+            dppCtrl = DppCtrl::DppRowXmask11;
+            break;
+          case 12:
+            dppCtrl = DppCtrl::DppRowXmask12;
+            break;
+          case 13:
+            dppCtrl = DppCtrl::DppRowXmask13;
+            break;
+          case 14:
+            dppCtrl = DppCtrl::DppRowXmask14;
+            break;
+          default:
+            canOptimize = false;
+            break;
+          }
+        }
+
+        // TODO: Check if we support dppmov.
+        if (canOptimize) {
+          Value dppCtrlVal = rewriter.create<LLVM::ConstantOp>(
+              loc, int32Type, (unsigned)dppCtrl);
+          Value rowMask = rewriter.create<LLVM::ConstantOp>(loc, int32Type, 15);
+          Value bankMask =
+              rewriter.create<LLVM::ConstantOp>(loc, int32Type, 15);
+          Value boundCtrl =
+              rewriter.create<LLVM::ConstantOp>(loc, boolType, true);
+          // TODO: Add a builder that directly takes int32_t.
+          Value shflValue = rewriter.create<ROCDL::MovDppOp>(
+              loc, int32Type, adaptor.getValue(), dppCtrlVal, rowMask, bankMask,
+              boundCtrl);
+          if (adaptor.getValue().getType().isF32()) {
+            shflValue = rewriter.create<LLVM::BitcastOp>(
+                loc, adaptor.getValue().getType(), shflValue);
+          }
+          // dstLane is always inside since we assume width to be 32.
+          Value isActiveSrcLane =
+              rewriter.create<LLVM::ConstantOp>(loc, boolType, true);
+          return std::make_pair(shflValue, isActiveSrcLane);
+        }
+
+        // TODO: Check if we support permlanedpp instructions.
+        if (mask >= 16) {
+          // For each mask from 16 to 31, this table contains the lane
+          // selection bits for the permlanex16 instruction.
+          static const unsigned laneSelBits[16][2] = {
+              {0x76543210, 0xfedcba98}, {0x67452301, 0xefcdab89},
+              {0x54761032, 0xdcfe98ba}, {0x45670123, 0xcdef89ab},
+              {0x32107654, 0xba98fedc}, {0x23016745, 0xab89efcd},
+              {0x10325476, 0x98badcfe}, {0x1234567, 0x89abcdef},
+              {0xfedcba98, 0x76543210}, {0xefcdab89, 0x67452301},
+              {0xdcfe98ba, 0x54761032}, {0xcdef89ab, 0x45670123},
+              {0xba98fedc, 0x32107654}, {0xab89efcd, 0x23016745},
+              {0x98badcfe, 0x10325476}, {0x89abcdef, 0x1234567}};
+          unsigned laneSelS1 = laneSelBits[mask - 16][0];
+          unsigned laneSelS2 = laneSelBits[mask - 16][1];
+
+          Value laneSelS1Value =
+              rewriter.create<LLVM::ConstantOp>(loc, int32Type, laneSelS1);
+          Value laneSelS2Value =
+              rewriter.create<LLVM::ConstantOp>(loc, int32Type, laneSelS2);
+          Value fi = rewriter.create<LLVM::ConstantOp>(loc, boolType, false);
+          Value boundCtrl =
+              rewriter.create<LLVM::ConstantOp>(loc, boolType, false);
+          Value shflValue = rewriter.create<ROCDL::PermlaneX16>(
+              loc, int32Type, adaptor.getValue(), laneSelS1Value,
+              laneSelS2Value, fi, boundCtrl);
+          if (adaptor.getValue().getType().isF32()) {
+            shflValue = rewriter.create<LLVM::BitcastOp>(
+                loc, adaptor.getValue().getType(), shflValue);
+          }
+          // dstLane is always inside since we assume width to be 32.
+          Value isActiveSrcLane =
+              rewriter.create<LLVM::ConstantOp>(loc, boolType, true);
+          return std::make_pair(shflValue, isActiveSrcLane);
+        }
+
+        // TODO: Use ds_swizzle here if permlanex16 instructions are not
+        // supported.
+      }
+    }
+
+    // Use default path.
+    const unsigned indexBitwidth = getTypeConverter()->getIndexTypeBitwidth();
+    Value srcLaneId = getLaneId(rewriter, loc, indexBitwidth);
+    Value dstLaneId =
+        rewriter.create<LLVM::XOrOp>(loc, int32Type, srcLaneId, offset);
+    return createShuffle(rewriter, loc, srcLaneId, dstLaneId, adaptor);
+  }
+
   /// Lowers a shuffle to the corresponding ROCDL ops.
   ///
   /// Use the `width` argument to see if src lane is participating.
@@ -136,50 +444,33 @@ struct GPUShuffleOpLowering : public ConvertOpToLLVMPattern<gpu::ShuffleOp> {
     // TODO: Add support for non 32-bit shuffle values.
     if (adaptor.getValue().getType().getIntOrFloatBitWidth() != 32)
       return failure();
-    const unsigned indexBitwidth = getTypeConverter()->getIndexTypeBitwidth();
-    Value srcLaneId = getLaneId(rewriter, loc, indexBitwidth);
 
     auto int32Type = IntegerType::get(rewriter.getContext(), 32);
+    Value offset = adaptor.getOffset();
     Value width = adaptor.getWidth();
-    Value zero = rewriter.create<LLVM::ConstantOp>(loc, int32Type, 0);
-    Value negwidth = rewriter.create<LLVM::SubOp>(loc, int32Type, zero, width);
-    Value add = rewriter.create<LLVM::AddOp>(loc, int32Type, srcLaneId, width);
-    Value widthOrZeroIfOutside =
-        rewriter.create<LLVM::AndOp>(loc, int32Type, add, negwidth);
-    Value dstLane;
+    const unsigned indexBitwidth = getTypeConverter()->getIndexTypeBitwidth();
+
     // TODO: Add support for gpu::ShuffleMode::UP and gpu::ShuffleMode::DOWN.
-    // TODO: Use ds_swizzle for XOR when step/offsets are constants for better
-    // perf.
+    Value shflValue, isActiveSrcLane;
     switch (op.getMode()) {
-    case gpu::ShuffleMode::XOR:
-      dstLane = rewriter.create<LLVM::XOrOp>(loc, int32Type, srcLaneId,
-                                             adaptor.getOffset());
+    case gpu::ShuffleMode::XOR: {
+      std::tie(shflValue, isActiveSrcLane) =
+          createShuffleXor(rewriter, loc, adaptor);
       break;
-    case gpu::ShuffleMode::IDX:
-      dstLane = adaptor.getOffset();
+    }
+    case gpu::ShuffleMode::IDX: {
+      Value srcLaneId = getLaneId(rewriter, loc, indexBitwidth);
+      std::tie(shflValue, isActiveSrcLane) =
+          createShuffle(rewriter, loc, srcLaneId, offset, adaptor);
       break;
-    default:
+    }
+    case gpu::ShuffleMode::UP:
+    case gpu::ShuffleMode::DOWN:
       return failure();
     }
-    Value isActiveSrcLane = rewriter.create<LLVM::ICmpOp>(
-        loc, LLVM::ICmpPredicate::slt, dstLane, widthOrZeroIfOutside);
-    Value selectDstLane = rewriter.create<LLVM::SelectOp>(loc, isActiveSrcLane,
-                                                          dstLane, srcLaneId);
-    Value two = rewriter.create<LLVM::ConstantOp>(loc, int32Type, 2);
-    Value dwordAlignedDstLane =
-        rewriter.create<LLVM::ShlOp>(loc, int32Type, selectDstLane, two);
-    Value initShflValue = adaptor.getValue();
-    if (adaptor.getValue().getType().isF32()) {
-      initShflValue =
-          rewriter.create<LLVM::BitcastOp>(loc, int32Type, initShflValue);
-    }
-    Value shflValue = rewriter.create<ROCDL::DsBpermuteOp>(
-        loc, int32Type, dwordAlignedDstLane, initShflValue);
-    if (adaptor.getValue().getType().isF32()) {
-      shflValue = rewriter.create<LLVM::BitcastOp>(
-          loc, adaptor.getValue().getType(), shflValue);
-    }
+
     rewriter.replaceOp(op, {shflValue, isActiveSrcLane});
+
     return success();
   }
 };
